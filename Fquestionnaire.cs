9ec8b5883136d5daa2ceb6cb411c477767a715cd
François Godin <francois.h.godin@gmail.com>
2018-04-19 13:48:45 +0200

    Add files via upload

---
+++ Fquestionnaire.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TestScholaNova
+{
+    public partial class Fquestionnaire : Form
+    {
+        private List<string> _questionsAlreadyAsked = new List<string>();
+        private static int _score;
+        private static int _tentative;
+        private double _resultat;
+        private Stopwatch watch = new Stopwatch();
+
+        public Fquestionnaire()
+        {
+            InitializeComponent();
+            tbProposition.MaxLength = 3;
+            _tentative = 0;
+            _score = 0;
+            btnStart.Click += BtnStart_Click;
+            btnValider.Click += BtnValider_Click;
+            tbProposition.KeyPress += TbProposition_KeyPress;
+            timerQuestionnaire.Tick += TimerQuestionnaire_Tick;
+            gbQuestion.Hide(); // je cache dans un 1er temps le groupBox question
+        }
+
+        private void TimerQuestionnaire_Tick(object sender, EventArgs e)
+        {
+            TimeSpan time = watch.Elapsed;
+            lbTimeValue.Text = time.ToString(@"mm\:ss\:ff");
+        }
+
+        /// <summary>
+        /// affiche la 1ere question et demarre le chrono
+        /// </summary>
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            btnStart.Hide(); 
+            lblEnonceText.Hide();
+            GetRandomOperation();
+            gbQuestion.Show(); // affichage question 
+            timerQuestionnaire.Start();
+            watch.Start();
+        }
+        /// <summary>
+        /// filtre la saisie du textBox et acepte seulement les chiffres et tirets
+        /// </summary>
+        private void TbProposition_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //controle de la saisie du champ de proposition
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar) &&(e.KeyChar != '-');
+        }
+
+        private void BtnValider_Click(object sender, EventArgs e)
+        {
+            _tentative = _tentative + 1;
+            if (tbProposition.Text == _resultat.ToString())
+                _score = _score + 1;
+
+            if (_tentative >= 10)
+            {
+                TimeSpan time = watch.Elapsed; //recupere le temps final 
+                btnValider.Enabled = false;
+                this.Hide(); // et on cache la current window
+                Fresultat fresultat = new Fresultat(_score,time.ToString(@"mm\:ss\:ff"),this);
+            }
+            else
+                GetRandomOperation();
+            tbProposition.Clear();
+        }
+        /// <summary>
+        /// initialise un objet question
+        /// controle si la question a déja été posée
+        /// set le label question 
+        /// recupere le resultat de la q
+        /// </summary>
+        private void GetRandomOperation()
+        { 
+            Question randomQuestion = new Question();
+
+            // si la question est déjà posée on en réinstancie une
+            while (_questionsAlreadyAsked.Contains(randomQuestion.ToString()))  
+                randomQuestion = new Question();
+
+            _resultat = randomQuestion.GetResultat(); // on set le resultat
+
+            _questionsAlreadyAsked.Add(randomQuestion.ToString()); //add la question a la liste des questions déjà posées
+            lblOperation.Text = randomQuestion.ToString(); // set la question dans le label question
+        }
+
+    }
+}
---
+++ Question.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestScholaNova
+{
+   public  class Question
+    {
+        int nb1;
+        int nb2;
+        char operateur;
+        Random _random = new Random();
+        static List<char> _lesOperateurs = new List<char> { '+', '-', '/', '*' };
+        static List<Division> _divisionsEntieres = GetDisvisionsEntieres();
+
+        public Question()
+        {
+            operateur = _lesOperateurs[_random.Next(0, 3)];
+            if (operateur == '/') // si c'est une division je prend une division entiere aléatoire de ma liste
+            {
+                int index = _random.Next(_divisionsEntieres.Count);
+                nb1 = _divisionsEntieres[index].nb1;
+                nb2 = _divisionsEntieres[index].nb2;
+            }
+            else // sinon je génère 2 arguments aléatoires
+            {
+                nb1 = _random.Next(0, 20);
+                nb2 = _random.Next(0, 20);
+            }
+        }
+
+        public double GetResultat()
+        {
+            switch (operateur)
+            {
+                case '+':
+                    return nb1 + nb2;
+                case '-':
+                    return nb1 - nb2;
+                case '/':
+                    return nb1 / Convert.ToDouble(nb2);
+                case '*':
+                    return  nb1 * nb2;
+                default:
+                    return  0;
+            }
+        }
+        /// <summary>
+        /// Genere une list d'objet division composé uniquement de division entière sur un intervale de nombre de 1 à 20
+        /// </summary>
+        private static List<Division> GetDisvisionsEntieres()
+        {
+            List<Division> listDiv = new List<Division>();
+            for (int i=1;i<20;i++)
+                for (int j = 2; j < 20; j++)
+                    if (i != j && i % j == 0)
+                    {
+                        var div = new Division { nb1 = i, nb2 = j };
+                        listDiv.Add((div));
+                    }
+            return listDiv;
+        }
+        public override string ToString()
+        {
+            return $"{nb1}{operateur}{nb2}";
+        }
+    }
+
+    public class Division
+    {
+        public int nb1 { get; set; }
+        public int nb2 { get; set; }
+    }
+}
